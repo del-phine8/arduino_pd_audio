fd4a41d37533e53ff584ca45d2d6f3698f112664
Del <delphinegagnon38@gmail.com>
2026-09-16 17:06:56 -0400

    ca clap avec 1bouton

--- arduino_pd_audio.ino
+++ arduino_pd_audio.ino
@@ -1,14 +1,27 @@
 // Le code minimal
 
-#include <Arduino.h> 
+#include <Arduino.h>
+#include <Bounce2.h>
 
-void setup() {
+Bounce2::Button bouton; // la variable du bouton
+
+#define BROCHE_BOUTON 2 // defini la broche sur le arduino qu'on nomme BROCHE_BOUTON
+
+void setup()
+{
   Serial.begin(115200);
-
+  bouton.setPressedState(LOW);
+  bouton.attach(BROCHE_BOUTON, INPUT_PULLUP); //tjr mettre pour setup le bouton
 }
 
-void loop() {
-Serial.print("bouton0 ");  // Envoie le descripteur et son espace en un seul bloc
-Serial.println(1);   // Envoie la valeur et ajoute automatiquement le saut de ligne final
+void loop()
+{
+  bouton.update(); // super important, tjr le mettre
 
+  if (bouton.pressed())
+  {
+    Serial.print("bouton1 "); // nomme le bouton pour différencier dans Pd, ecrire "route bouton1" fait en sorte que ca ignore bouton1 et print directement 1
+    Serial.print(1);
+    Serial.println();
+  }
 }
